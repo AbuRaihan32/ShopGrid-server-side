c61c0492747fe7a86e2a82583e2fe747b3f47e6a
AbuRaihan32 <abumahfuz3211@gmail.com>
2024-08-16 21:27:25 +0600

    basic setup complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+// ! middlewares
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("shopGrid server is running");
+});
+
+app.listen(port, () => {
+  console.log(`shopGrid is running in port ${port}`);
+});
